1cfb86fa9f0a37b00ce139144b3ce00b8ca0876e
JakubSzczech3 <116097926+JakubSzczech3@users.noreply.github.com>
2024-01-17 13:19:05 +0100

    Add files via upload

---
+++ Wczytywanie/ConsoleApp7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj numer albumu: ");
+            string numerAlbumu = Console.ReadLine();
+            Console.WriteLine("Podaj nazwę pliku: ");
+            string nazwaPliku = Console.ReadLine();
+
+            using (var sw = new StreamWriter(nazwaPliku, append: true))
+            {
+
+                sw.WriteLine(numerAlbumu);
+            }
+            using (var sr = new StreamReader(nazwaPliku))
+            {
+                var line = sr.ReadToEnd();
+                Console.WriteLine(line);
+            }
+
+            
+
+
+        }
+    }
+}
